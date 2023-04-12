70b62e4b0a7a8721dcbbcb40267d9f6ee0a72488
certainity <41723813+certainity@users.noreply.github.com>
2023-04-12 13:10:32 +0500

    Update automate_google_sheet.py

--- automate_google_sheet.py
+++ automate_google_sheet.py
@@ -1,24 +1,112 @@
+# streamlit_app.py
+
 import streamlit as st
-import pandas as pd #if you will
-import gspread
-from google.oauth2 import service_account
+import pandas as pd
+import geopandas as gpd
+import pydeck as pdk
+import leafmap
+from gsheetsdb import connect
+from shapely.geometry import Polygon, mapping
+
+st.set_page_config(page_title="UT Parking", layout="wide")
+
+# Remove the sandwich menu in the upper right corner
+hide_streamlit_style = """
+            <style>
+            # MainMenu {visibility: hidden;}
+            header, footer {visibility: hidden;}
+            div.block-container {padding-top:1rem;padding-left:2rem;padding-right:2rem;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+st.title("University of Tennessee Parking Lot Availability")
+
+col1, col2 = st.columns([1, 3])
+
+data = "data/buildings.geojson"
+
+gdf = gpd.read_file(data)
+gdf = gdf.astype({"osmid": int})
 
 # Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"
-    ],
+conn = connect()
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+
+
+@st.cache(ttl=60)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+
+# sheet_url = st.secrets["public_gsheets_url"]
+# rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# # Print results.
+# for row in rows:
+#     st.write(f"{row.name} has a :{row.pet}:")
+
+sheet_url = st.secrets["public_gsheets_parking"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+df = pd.DataFrame(rows)
+df = df.astype({"osmid": int, "capacity": int, "remaining": int})
+gdf2 = pd.merge(gdf, df[["osmid", "capacity", "remaining"]], on="osmid", how="outer")
+with col1:
+    st.header("Parking Lots")
+    extruded = st.checkbox("3D View", value=False)
+    st.dataframe(df)
+
+    st.info(
+        """
+        Source code:
+        [https://github.com/giswqs/streamlit-google-sheet](https://github.com/giswqs/streamlit-google-sheet)
+        """
+    )
+
+    st.image("https://i.imgur.com/NcDcIWa.png")
+
+
+layer = pdk.Layer(
+    "GeoJsonLayer",
+    gdf2,
+    pickable=True,
+    opacity=0.051,
+    filled=True,
+    stroked=True,
+    extruded=extruded,
+    wireframe=True,
+    get_elevation="remaining",
+    get_line_color=[255, 255, 0],
+    get_fill_color=[165, 42, 42],
+    get_line_width=1,
+    line_width_min_pixels=1,
 )
-conn = connect(credentials=credentials)
-client=gspread.authorize(credentials)
 
-sheet_id = 'https://docs.google.com/spreadsheets/d/179l487cHKrk6EXJ0oh7Q6u4sqlZ-5H8Z3E-JyquXeuQ/edit?usp=sharing'
-csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
-database_df = pd.read_csv(csv_url, on_bad_lines='skip')
+tooltip = {
+    "text": "osmid: {osmid}\nbuilding: {building}\nname: {name}\namenity: {amenity}\ncapacity: {capacity}\nremaining: {remaining}",
+}
 
-database_df = database_df.astype(str)
-sheet_url = st.secrets["private_gsheets_url"] #this information should be included in streamlit secret
-sheet = client.open_by_url(sheet_url).sheet1
-sheet.update([database_df.columns.values.tolist()] + database_df.values.tolist())
-st.success('Data has been written to Google Sheets')
+with col2:
+    st.pydeck_chart(
+        pdk.Deck(
+            # map_style=None,
+            map_provider="mapbox",
+            # map_style=pdk.map_styles.SATELLITE,
+            initial_view_state=pdk.ViewState(
+                latitude=35.951955,
+                longitude=-83.930826,
+                zoom=15,
+                pitch=0,
+                height=800,
+            ),
+            layers=[
+                layer,
+            ],
+            tooltip=tooltip,
+        )
+    )
