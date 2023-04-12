b1249716ce22bd3095edde5030ff9bdfb46ab272
certainity <41723813+certainity@users.noreply.github.com>
2023-04-12 13:07:40 +0500

    Update automate_google_sheet.py

--- automate_google_sheet.py
+++ automate_google_sheet.py
@@ -1,7 +1,18 @@
 import streamlit as st
-import pandas as pd
+import pandas as pd #if you will
+import gspread
+from google.oauth2 import service_account
 
-# Dump any DataFrame
-d = {'Type': ['Notebook', 'DVDs'], 'Quantity': [1, 2], 'Price': [400, 200]}
-df = pd.DataFrame(data=d)
-st.dataframe(df)
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"
+    ],
+)
+conn = connect(credentials=credentials)
+client=gspread.authorize(credentials)
+
+sheet_id = 'https://docs.google.com/spreadsheets/d/179l487cHKrk6EXJ0oh7Q6u4sqlZ-5H8Z3E-JyquXeuQ/edit?usp=sharing'
+csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
+database_df = pd.read_csv(csv_url, on_bad_lines='skip')
